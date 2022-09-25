1745b369f3bcbd7c18ecd249fd6437d63fced15f
주현태 <jabel123@naver.com>
2022-09-25 12:40:35 +0900

    chap06: 암호화 추가 예제
    암호화 추가 예제

--- chap06/example/gzip-receive.js
+++ chap06/example/gzip-receive.js
@@ -2,14 +2,19 @@ import { createServer } from 'http'
 import { createWriteStream } from 'fs'
 import { createGunzip } from 'zlib'
 import { basename, join } from 'path'
+import { createDecipheriv, randomBytes } from 'crypto'
+
+const secret = randomBytes(24)
+console.log(`Generated secret: ${secret.toString('hex')}`)
 
 const server = createServer((req, res) => {
   const filename = basename(req.headers['x-filename'])
-  console.log('filename ->', filename)
+  const iv = Buffer.from(req.headers['x-initialization-vector'], 'hex')
   const destFilename = join('./received_files', filename)
+
   console.log(`File request received: ${filename}`)
-  console.log('destfilename ->', destFilename)
   req
+    .pipe(createDecipheriv('aes192', secret, iv))
     .pipe(createGunzip())
     .pipe(createWriteStream(destFilename))
     .on('finish', () => {
--- chap06/example/gzip-send.js
+++ chap06/example/gzip-send.js
@@ -2,9 +2,12 @@ import { request } from 'http'
 import { createGzip } from 'zlib'
 import { createReadStream } from 'fs'
 import { basename } from 'path'
+import { createCipheriv, randomBytes } from 'crypto' // 먼저 crypto 모듈에서 createSipheriv() transform 스트림과 randomBytes() 함수를 임포트합니다.
 
 const filename = process.argv[2]
 const serverHost = process.argv[3]
+const secret = Buffer.from(process.argv[4], 'hex')
+const iv = randomBytes(16)
 
 const httpRequestOptions = {
   hostname: serverHost,
@@ -14,7 +17,8 @@ const httpRequestOptions = {
   headers: {
     'Content-Type': 'application/octet-stream',
     'Content-Encoding': 'gzip',
-    'X-Filename': basename(filename)
+    'X-Filename': basename(filename),
+    'X-Initialization-Vector': iv.toString('hex') // 초기화 벡터를 HTTP 헤더로 서버에 전달합니다.
   }
 }
 
@@ -24,7 +28,8 @@ const req = request(httpRequestOptions, (res) => {
 
 createReadStream(filename)
   .pipe(createGzip())
-  .pipe(req)
+  .pipe(createCipheriv('aes192', secret, iv))
+  .pipe(req)  // Gzip 단계 이후 데이터를 암호화합니다.
   .on('finish', () => {
     console.log('File successfully sent')
   })
